d09f0017f673f6cb6886c22a40e0efce5cb2feb8
Sarah Ibrahim <saraibrahim4121@gmail.com>
2022-08-05 16:01:23 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main=document.querySelector("main");
+main.remove();
+
+const newHeader=document.createElement('h1');
+newHeader.setAttribute("id","victory");
+
+newHeader.textContent="Sarah is the champion";
